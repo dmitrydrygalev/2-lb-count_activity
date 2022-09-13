3d545ea6ac59b7965eca7b7690990e4ed10c780e
WEB-MASTER(DD) <campermailru@mail.ru>
2022-09-13 11:54:16 +0300

    Подсчет дистации и каллорий(finished)

--- main.py
+++ main.py
@@ -5,12 +5,18 @@ activeTime = int(input("-| Сколько заняла времени трени
 
 
 def countTrainingCost(M, H, S, T):
-
     distance = S * (H / 4 + 0.37)
-    speed = int(distance/T)
+    speed = int(distance / T)
     energy = int(0.035 * M + (speed ** 2 / H) * 0.029 * M)
 
-    return distance, energy
+    if distance <= 2000:
+        motivateText = ' Продолжаем в том же духе! '
+    elif distance <= 4000:
+        motivateText = ' Ого мы уже ушли так далеко, не время останавливаться '
+    elif distance > 4000:
+        motivateText = ' Вы молодец! Невероятная дистанция! '
+
+    return distance, energy, motivateText
 
 
 print(f"Дистанция(м) - калории:{countTrainingCost(mass, height, steps, activeTime)}")
